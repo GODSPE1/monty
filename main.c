937f5ce9909eec6e0276ecc09993e845a384a89c
GODSPE1 <godspy18@gmail.com>
2023-12-13 20:12:49 -0800

    create the entry point main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+
+return (0);
+}
