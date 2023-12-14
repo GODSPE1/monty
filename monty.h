cc2b93a3cb49d23297da3cedaaf5c4f6163630d7
Mr-Michael_dev <michael.oyedepo@gmail.com>
2023-12-14 22:39:47 +0100

    debug code

--- monty.h
+++ monty.h
@@ -35,4 +35,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+char **tokenize(char *line, char *delim);
+
+void free_mem(char **arg);
+
 #endif
---
+++ token.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * tokenize - tokenize a char string into array of strings
+ * @line: string to be tokenized
+ * @delim: delimeter
+ *
+ * Return: array of strings a
+ */
+
+char **tokenize(char *line, char *delim)
+{
+	char **arg, *token;
+	int i = 0;
+
+	arg = (char **)malloc(101 * sizeof(char *));
+
+	token = strtok(line, delim);
+
+	while (token != NULL)
+	{
+		arg[i] = malloc(strlen(token) + 1);
+		strcpy(arg[i++], token);
+		token = strtok(NULL, delim);
+	}
+	arg[i] = NULL;
+	i = 0;
+
+	return (arg);
+}
