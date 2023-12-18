5c1150e2cccee0e9d4e589a9cb3b49a7af4b5589
GODSPE1 <godspy18@gmail.com>
2023-12-17 16:09:04 -0800

    add div and mod function

--- monty_functions2.c
+++ monty_functions2.c
@@ -54,3 +54,62 @@ void mul(stack_t **stack, unsigned int line_num)
     pop(stack, line_num);
     count -= 1;
 }
+
+/**
+ * mod - calculates the modulus of the second value from the top
+ * @stack: a pointer to the top node stack_t structure
+ * @line_num: line number where the mod opcode is from
+ *
+ * Return: Nothing.
+ */
+void mod(stack_t **stack, unsigned int line_num)
+{
+    stack_t *tmp1, *tmp2;
+
+    if ((*stack)->next == NULL || (*stack)->next->next == NULL || count < 2)
+    {
+        printf("L%d: can't mod, stack too short\n", line_num);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp1 = (*stack)->next;
+    tmp2 = tmp1->next;
+
+    tmp1->n = tmp2->n % tmp1->n;
+
+    pop(stack, line_num);
+    count -= 1;
+}
+
+/**
+ * div - divides the top two elements of the stack
+ * @stack: a pointer to the top node stack_t structure
+ * @line_num: line number where the div opcode is from
+ *
+ * Return: Nothing.
+ */
+void div(stack_t **stack, unsigned int line_num)
+{
+    stack_t *tmp1, *tmp2;
+
+    if (count < 2)
+    {
+        printf("L%d: can't div, stack too short\n", line_num);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp1 = (*stack)->next;
+    tmp2 = tmp1->next;
+
+    if (tmp1->n == 0)
+    {
+        printf("L%d: division by zero\n", line_num);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp2->n = tmp2->n / tmp1->n;
+
+    pop(stack, line_num);
+    count -= 1;
+}
+
